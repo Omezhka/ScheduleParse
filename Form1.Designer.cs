85e3d2dbd7e4f9931afeb5658ac84aa020ac6228
Наталья Аверкова <falabellla-61@mail.ru>
2021-06-12 16:54:25 +0300

    добавление кнопки "Найти"

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -49,18 +49,20 @@ namespace ScheduleParse
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonFind = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItemUpdate,
             this.формированиеРасписанияНаСтендToolStripMenuItem,
             this.расписаниеПреподовToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(526, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(701, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuUpdate";
             // 
@@ -71,26 +73,26 @@ namespace ScheduleParse
             this.toolStripMenuItemExtraStud,
             this.toolStripMenuItemMagistr});
             this.toolStripMenuItemUpdate.Name = "toolStripMenuItemUpdate";
-            this.toolStripMenuItemUpdate.Size = new System.Drawing.Size(156, 20);
+            this.toolStripMenuItemUpdate.Size = new System.Drawing.Size(198, 24);
             this.toolStripMenuItemUpdate.Text = "Обновление расписания";
             // 
             // toolStripMenuItemFullTimeEdu
             // 
             this.toolStripMenuItemFullTimeEdu.Name = "toolStripMenuItemFullTimeEdu";
-            this.toolStripMenuItemFullTimeEdu.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemFullTimeEdu.Size = new System.Drawing.Size(224, 26);
             this.toolStripMenuItemFullTimeEdu.Text = "Очная форма";
             this.toolStripMenuItemFullTimeEdu.Click += new System.EventHandler(this.toolStripMenuItemFullTimeEdu_Click);
             // 
             // toolStripMenuItemExtraStud
             // 
             this.toolStripMenuItemExtraStud.Name = "toolStripMenuItemExtraStud";
-            this.toolStripMenuItemExtraStud.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemExtraStud.Size = new System.Drawing.Size(224, 26);
             this.toolStripMenuItemExtraStud.Text = "Заочная форма";
             // 
             // toolStripMenuItemMagistr
             // 
             this.toolStripMenuItemMagistr.Name = "toolStripMenuItemMagistr";
-            this.toolStripMenuItemMagistr.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemMagistr.Size = new System.Drawing.Size(224, 26);
             this.toolStripMenuItemMagistr.Text = "Магистратура";
             // 
             // формированиеРасписанияНаСтендToolStripMenuItem
@@ -98,72 +100,78 @@ namespace ScheduleParse
             this.формированиеРасписанияНаСтендToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItem1});
             this.формированиеРасписанияНаСтендToolStripMenuItem.Name = "формированиеРасписанияНаСтендToolStripMenuItem";
-            this.формированиеРасписанияНаСтендToolStripMenuItem.Size = new System.Drawing.Size(221, 20);
+            this.формированиеРасписанияНаСтендToolStripMenuItem.Size = new System.Drawing.Size(282, 24);
             this.формированиеРасписанияНаСтендToolStripMenuItem.Text = "Формирование расписания на стенд";
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
             this.toolStripMenuItem1.Text = "Сформировать";
             // 
             // расписаниеПреподовToolStripMenuItem
             // 
             this.расписаниеПреподовToolStripMenuItem.Name = "расписаниеПреподовToolStripMenuItem";
-            this.расписаниеПреподовToolStripMenuItem.Size = new System.Drawing.Size(140, 20);
+            this.расписаниеПреподовToolStripMenuItem.Size = new System.Drawing.Size(178, 24);
             this.расписаниеПреподовToolStripMenuItem.Text = "Расписание преподов";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(260, 405);
+            this.label1.Location = new System.Drawing.Point(346, 484);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.Size = new System.Drawing.Size(223, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Дата обновления очной формы:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(448, 405);
+            this.label2.Location = new System.Drawing.Point(596, 484);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.Size = new System.Drawing.Size(80, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "04.06.2021";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(260, 431);
+            this.label3.Location = new System.Drawing.Point(346, 516);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(174, 13);
+            this.label3.Size = new System.Drawing.Size(225, 17);
             this.label3.TabIndex = 3;
             this.label3.Text = "Дата обновления магистратуры:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(260, 418);
+            this.label4.Location = new System.Drawing.Point(346, 500);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(182, 13);
+            this.label4.Size = new System.Drawing.Size(238, 17);
             this.label4.TabIndex = 4;
             this.label4.Text = "Дата обновления заочной формы:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(448, 431);
+            this.label5.Location = new System.Drawing.Point(596, 516);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.Size = new System.Drawing.Size(80, 17);
             this.label5.TabIndex = 5;
             this.label5.Text = "04.06.2021";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(448, 418);
+            this.label6.Location = new System.Drawing.Point(596, 500);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.Size = new System.Drawing.Size(80, 17);
             this.label6.TabIndex = 6;
             this.label6.Text = "04.06.2021";
             // 
@@ -171,9 +179,10 @@ namespace ScheduleParse
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(163, 24);
+            this.label7.Location = new System.Drawing.Point(217, 30);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(174, 24);
+            this.label7.Size = new System.Drawing.Size(227, 29);
             this.label7.TabIndex = 7;
             this.label7.Text = "Поиск расписания";
             // 
@@ -181,9 +190,10 @@ namespace ScheduleParse
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label8.Location = new System.Drawing.Point(21, 61);
+            this.label8.Location = new System.Drawing.Point(28, 75);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(155, 20);
+            this.label8.Size = new System.Drawing.Size(191, 25);
             this.label8.TabIndex = 8;
             this.label8.Text = "Введите фамилию";
             // 
@@ -191,25 +201,28 @@ namespace ScheduleParse
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label9.Location = new System.Drawing.Point(21, 104);
+            this.label9.Location = new System.Drawing.Point(28, 128);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(125, 20);
+            this.label9.Size = new System.Drawing.Size(153, 25);
             this.label9.TabIndex = 9;
             this.label9.Text = "Выберите дату";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(199, 103);
+            this.dateTimePicker1.Location = new System.Drawing.Point(265, 127);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(310, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(225, 22);
             this.dateTimePicker1.TabIndex = 11;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(25, 144);
+            this.textBox2.Location = new System.Drawing.Point(33, 177);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(484, 247);
+            this.textBox2.Size = new System.Drawing.Size(644, 303);
             this.textBox2.TabIndex = 12;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
@@ -225,17 +238,30 @@ namespace ScheduleParse
             "киви",
             "маракуйя",
             "петя"});
-            this.comboBox1.Location = new System.Drawing.Point(199, 60);
+            this.comboBox1.Location = new System.Drawing.Point(265, 74);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(310, 21);
+            this.comboBox1.Size = new System.Drawing.Size(412, 24);
             this.comboBox1.Sorted = true;
             this.comboBox1.TabIndex = 13;
             // 
+            // buttonFind
+            // 
+            this.buttonFind.AutoSize = true;
+            this.buttonFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFind.Location = new System.Drawing.Point(510, 124);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(167, 30);
+            this.buttonFind.TabIndex = 14;
+            this.buttonFind.Text = "Найти";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(526, 450);
+            this.ClientSize = new System.Drawing.Size(701, 537);
+            this.Controls.Add(this.buttonFind);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.dateTimePicker1);
@@ -250,6 +276,7 @@ namespace ScheduleParse
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
@@ -281,6 +308,7 @@ namespace ScheduleParse
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonFind;
     }
 }
 
